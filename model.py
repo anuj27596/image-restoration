86f2a6f98481d48669e7627b36686f2e3f9b24db
anuj27596 <55323857+anuj27596@users.noreply.github.com>
2020-05-20 15:49:38 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,45 @@
+from keras.layers import *
+from keras.models import Model
+
+def conv2d_block(input_tensor, n_filters, kernel_size=3):
+
+	x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), padding="same")(input_tensor)
+	x = Activation("softplus")(x)
+
+	x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), padding="same")(x)
+	x = Activation("softplus")(x)
+
+	return x
+
+def get_Model(input_img, n_filters=16):
+
+	c1 = conv2d_block(input_img, n_filters=n_filters*1, kernel_size=3)
+	p1 = MaxPooling2D((2, 2)) (c1)
+
+	c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3)
+	p2 = MaxPooling2D((2, 2)) (c2)
+
+	c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3)
+	p3 = MaxPooling2D((2, 2)) (c3)
+
+	c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3)
+	p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+	
+	c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3)
+	
+	u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
+	c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3)
+
+	u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
+	c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3)
+
+	u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
+	c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3)
+
+	u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
+	c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3)
+	
+	outputs = Conv2D(3, (1, 1), activation='sigmoid') (c9)
+	model = Model(inputs=[input_img], outputs=[outputs])
+
+	return model
---
+++ text_removal.py
@@ -0,0 +1,58 @@
+from keras.layers import Input
+from keras.callbacks import Callback
+from keras.optimizers import *
+import keras.backend as K
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from model import get_Model
+
+
+class BMCallback(Callback):
+	def __init__(self):
+		self.best_weights = None
+		self.min_loss = 1
+
+	def on_epoch_end(self, epoch, logs=None):
+		if logs['loss'] < self.min_loss:
+			self.min_loss = logs['loss']
+			self.best_weights = self.model.get_weights()
+
+def mse_masked(mask):
+	def loss_func(y_true, y_pred):
+		return K.mean(K.square((y_pred - y_true) * mask))
+	return loss_func
+
+
+img = plt.imread('data/cat.png')[:,:,:3]
+M, N, C = img.shape
+
+mask = (np.sum(img, axis=2) > 0).reshape(M, N, 1)	# pixels of text are (0,0,0) (black)
+
+F = 16
+
+model = get_Model(Input((M, N, F)), n_filters=16)
+model.compile(optimizer=Adam(), loss=mse_masked(mask))
+
+z = np.random.uniform(low=-1, high=1, size=(1,M,N,F))
+
+bmcb = BMCallback()
+
+model.fit(z, img.reshape((1,M,N,C)), epochs=2000, verbose=1, callbacks=[bmcb])
+model.set_weights(bmcb.best_weights)
+
+j = model.predict(z)
+
+plt.figure()
+
+plt.subplot(2,1,1)
+plt.imshow(img)
+
+plt.subplot(2,1,2)
+plt.imshow(j[0])
+
+plt.figure()
+plt.plot(model.history.history['loss'])
+
+plt.show()
