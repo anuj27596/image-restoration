c742a9ef2af0b03439f451a9ac86d02cf2dce853
anuj27596 <55323857+anuj27596@users.noreply.github.com>
2020-06-07 11:01:40 +0530

    Update model & add Denoising

---
+++ denoising.py
@@ -0,0 +1,54 @@
+from keras.layers import Input
+from keras.optimizers import Adam
+import keras.backend as K
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from model import get_Model
+
+def mse_gr(gr):
+	def loss_func(y_true, y_pred):
+		return K.mean(K.square(y_pred - gr))
+	return loss_func
+
+gr_img = plt.imread('data/car.png')[:,:,:3]
+M, N, C = gr_img.shape
+
+sigma = 0.1
+np.random.seed(10)
+noisy_img = gr_img + sigma * np.random.randn(M, N, C)
+noisy_img = np.vectorize(lambda x: 1.0 if x>1 else 0.0 if x<0 else x)(noisy_img)
+
+F = 16
+
+tf.set_random_seed(100)
+
+model = get_Model(Input((M, N, F)), n_filters=8, depth=4, output_channels=C)
+model.compile(optimizer=Adam(), loss='mse', metrics=[mse_gr(gr_img)])
+
+np.random.seed(2)
+z = np.random.uniform(low=-1, high=1, size=(1,M,N,F))
+
+
+model.fit(z, noisy_img.reshape((1,M,N,C)), epochs=900, verbose=1)
+
+j = model.predict(z)
+
+plt.figure()
+
+plt.subplot(3,1,1)
+plt.imshow(noisy_img)
+
+plt.subplot(3,1,2)
+plt.imshow(j[0])
+
+plt.subplot(3,1,3)
+plt.imshow(gr_img)
+
+plt.figure()
+plt.plot(model.history.history['loss'])
+
+plt.show()
+
--- model.py
+++ model.py
@@ -11,35 +11,48 @@ def conv2d_block(input_tensor, n_filters, kernel_size=3):
 
 	return x
 
-def get_Model(input_img, n_filters=16):
+def get_Model(input_img, n_filters=16, depth=4, output_channels=3):
 
-	c1 = conv2d_block(input_img, n_filters=n_filters*1, kernel_size=3)
-	p1 = MaxPooling2D((2, 2)) (c1)
+	l = conv2d_block(input_img, n_filters=n_filters, kernel_size=3)
+	l = MaxPooling2D((2, 2)) (l)
 
-	c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3)
-	p2 = MaxPooling2D((2, 2)) (c2)
+	for i in range(1,depth):
+		l = conv2d_block(l, n_filters=n_filters * 2**i, kernel_size=3)
+		l = MaxPooling2D((2, 2)) (l)
 
-	c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3)
-	p3 = MaxPooling2D((2, 2)) (c3)
+	l = conv2d_block(l, n_filters=n_filters * 2**depth, kernel_size=3)
 
-	c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3)
-	p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+	for i in range(depth):
+		l = Conv2DTranspose(n_filters * 2**(depth-i-1), (3, 3), strides=(2, 2), padding='same') (l)
+		l = conv2d_block(l, n_filters=n_filters*8, kernel_size=3)
+
+	# c1 = conv2d_block(input_img, n_filters=n_filters*1, kernel_size=3)
+	# p1 = MaxPooling2D((2, 2)) (c1)
+
+	# c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3)
+	# p2 = MaxPooling2D((2, 2)) (c2)
+
+	# c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3)
+	# p3 = MaxPooling2D((2, 2)) (c3)
+
+	# c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3)
+	# p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
 	
-	c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3)
+	# c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3)
 	
-	u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
-	c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3)
+	# u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
+	# c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3)
 
-	u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
-	c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3)
+	# u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
+	# c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3)
 
-	u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
-	c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3)
+	# u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
+	# c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3)
 
-	u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
-	c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3)
+	# u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
+	# c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3)
 	
-	outputs = Conv2D(3, (1, 1), activation='sigmoid') (c9)
+	outputs = Conv2D(output_channels, (1, 1), activation='sigmoid') (l)
 	model = Model(inputs=[input_img], outputs=[outputs])
 
 	return model
