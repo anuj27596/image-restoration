abc7446e278f8b80c4fe0864fabcf0acc3c160c5
taufeeque9 <9taufeeque9@gmail.com>
2020-06-25 20:14:43 +0530

    added denoising

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-from keras.layers import *
-from keras.models import Model
 import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
 
 def conv2d_block(input_tensor, n_filters, kernel_size=3):
 
@@ -28,7 +28,7 @@ def get_Model(input_img, n_filters=16, depth=4, res_exp=0, output_channels=3):
 		l = Conv2DTranspose(int(n_filters * 2**(depth-i-1)), (3, 3), strides=(2, 2), padding='same') (l)
 		l = conv2d_block(l, n_filters=int(n_filters * 2**(depth-i-1)), kernel_size=3)
 
-	
+
 	outputs = Conv2D(output_channels, (1, 1), activation='sigmoid') (l)
 	model = Model(inputs=[input_img], outputs=[outputs])
 
